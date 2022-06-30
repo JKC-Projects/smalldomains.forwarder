03eba68416caa4a38677b52ddcaa0a5955629faa
JK-Chung <chungjeeken@outlook.com>
2022-06-30 22:21:09 +0100

    SDOMAINS-4 Adding more tests

--- main_test.go
+++ main_test.go
@@ -21,3 +21,21 @@ func TestExtractsLastPathComponent(t *testing.T) {
 		t.Errorf("SmallDomain of second incorrectly extracted from path: %v", path)
 	}
 }
+
+func TestSmallDomainsAllowsHyphens(t *testing.T) {
+	const path = "test-this-small-domain"
+	extractedSmallDomain := extractSmallDomainAliasFromPath(path)
+
+	if extractedSmallDomain != path {
+		t.Errorf("SmallDomain incorrectly disallows hyphens: %v", path)
+	}
+}
+
+func TestSmallDomainsAllowsUnderscores(t *testing.T) {
+	const path = "test_this_small_domain"
+	extractedSmallDomain := extractSmallDomainAliasFromPath(path)
+
+	if extractedSmallDomain != path {
+		t.Errorf("SmallDomain incorrectly disallows underscores: %v", path)
+	}
+}
